8f9e515a3aa8ac9451a419741f6d9b9b1e757732
Zhangir <Appelhd1337@gmail.com>
2023-06-20 11:35:22 +0600

    adding New MainPage

---
+++ src/components/CarouselControl.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="carousel-indicator">
+        <button class="carousel-indicator-item active"></button>
+        <button class="carousel-indicator-item"></button>
+        <button class="carousel-indicator-item"></button>
+        <button class="carousel-indicator-item"></button>
+        <button class="carousel-indicator-item"></button>
+    </div>
+</template>
+
+<script>
+export default{
+
+}
+</script>
+
+<style scoped>
+.carousel-indicator{
+    position: absolute;
+    transform: translateX(-46%);
+    left: 51%;
+    z-index: 2;
+}
+
+.carousel-indicator-item{
+    width: 15px;
+    height: 15px;
+    border: none;
+    margin: 0.2em;
+    border-radius: 50%;
+    cursor: pointer;
+}
+
+.active{
+    background: #999;
+}
+</style>
---
+++ src/components/СarouselItem.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="v-carouselItem">
+      <img :src="carousel_data.img" alt="">
+      <div class="carouselText">
+        <div class="textWrapper">
+          <div class="carouselTitleText" :style="{ color: carousel_data.textColor }">{{ carousel_data.title }}</div>
+          <router-link class="shopText" to="/productList" :style="{ color: carousel_data.textColor }">Shop Now</router-link>
+        </div>
+      </div>
+    </div>
+</template>
+  
+<script>
+  export default {
+    props: {
+      carousel_data: {
+        type: Object,
+        required: true
+      }
+    },
+    methods: {
+      resolveImage() {
+        return this.carousel_data.img;
+      }
+    }
+  };
+</script>
+  
+<style scoped>
+.v-carouselItem {
+  position: relative;
+}
+
+.carouselText {
+  position: absolute;
+}
+
+.carouselTitleText {
+  font-size: 50px;
+  font-weight: 900;
+  margin-bottom: 20px;
+}
+
+.shopText {
+  font-weight: 700;
+  font-size: 18px;
+}
+
+.textWrapper{
+    position:absolute;
+    width: 420px;
+    left: 65px;
+    top: -200px;
+    bottom: 129px;
+}
+
+
+</style>
+  
